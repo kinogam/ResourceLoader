a2b7231fba33d63c3af331c7a525882ac9053e1c
kinogam <kinogam@gmail.com>
2011-03-25 10:11:16 +0800

    full one

---
+++ Script/ResourceLoader.js
@@ -0,0 +1,112 @@
+﻿(function () {
+    var rl = window.ResourceLoader = function () {
+        this.resourceList = new Array();
+        this.onload = function () { };
+        this.itemLoad = function () { };
+        this.loadedCount = 0;
+        this.rs = {};
+    };
+
+    rl.prototype.add = function (s) {
+        this.resourceList.push(s);
+    }
+
+    rl.prototype.load = function () {
+        if (this.resourceList.length == 0) {
+            this.onload.call();
+            return;
+        }
+        for (var i = 0; i < this.resourceList.length; i++) {
+            var type = this.resourceList[i].type;
+            type = type.substr(0, 1).toUpperCase() + type.substr(1, type.length - 1).toLowerCase();
+            rl["handle" + type].call(this.resourceList[i], this, this.itemLoad);
+        }
+    }
+
+
+
+    rl.handleImage = function (rsobj, callback) {
+        var itemobj = this;
+        var img = new Image();
+        itemobj.media = img;
+        rsobj.rs[itemobj.key] = img;
+
+        img.onerror = function () {
+            rl.itemFinishHandle(rsobj, itemobj, callback, false);
+        }
+
+        img.onload = function () {
+            rl.itemFinishHandle(rsobj, itemobj, callback, true);
+        }
+
+        img.src = itemobj.src;
+    }
+
+    rl.handleAudio = function (rsobj, callback) {
+        var itemobj = this;
+        var audio = new Audio();
+        itemobj.media = audio;
+        rsobj.rs[itemobj.key] = audio;
+        audio.src = itemobj.src;
+        rl.mediaCheck(rsobj, itemobj, callback);
+    }
+
+    rl.handleVideo = function (rsobj, callback) {
+        var itemobj = this;
+        var video = document.createElement('video');
+        itemobj.media = video;
+        rsobj.rs[itemobj.key] = video;
+        video.src = itemobj.src;
+        rl.mediaCheck(rsobj, itemobj, callback);
+    }
+
+    rl.mediaCheck = function (rsobj, itemobj, callback) {
+        var media = itemobj.media;
+        if (media.error != null || media.readyState == 4) {
+            var isSuccess = (media.readyState == 4 && media.error == null) ? true : false;
+            try {
+                rl.itemFinishHandle(rsobj, itemobj, callback, isSuccess);
+            }
+            finally {
+                return;
+            }
+        }
+
+        setTimeout(function () {
+            rl.mediaCheck(rsobj, itemobj, callback);
+        }, 20);
+    }
+
+
+    rl.itemFinishHandle = function (rsobj, itemobj, callback, isSuccess) {
+        rsobj.loadedCount++;
+
+        if (callback != null)
+            callback.call(itemobj, isSuccess);
+
+        if (rsobj.loadedCount == rsobj.resourceList.length)
+            rsobj.onload.call();
+    }
+
+
+
+    rl.prototype.clearItem = function (key) {
+        this.rs[key] = null;
+        delete this.rs[key];
+        for (var i = 0; i < this.resourceList.length; i++) {
+            if (this.resourceList[i].key == key) {
+                this.resourceList.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    rl.prototype.clearAll = function () {
+        for (var i in this.rs) {
+            this.rs[i] = null;
+            delete this.rs[i];
+        }
+        this.resourceList = new Array();
+    }
+
+})();
---
+++ Script/ResourceLoaderTest.js
@@ -0,0 +1,81 @@
+﻿/// <reference path="../../qunit/qunit.js" />
+/// <reference path="ResourceLoader.js" />
+
+test("resource list length test", function () {
+    var rl = new ResourceLoader();
+    rl.add({ type: "image", key: "pic1", src: "1.jpg" });
+    equal(rl.resourceList.length, 1);
+    rl.add({ type: "audio", key: "audio1", src: "01.mp3" }); 
+    equal(rl.resourceList.length, 2);
+});
+
+
+
+asyncTest("resource list items callback test", function () {
+    var rl = new ResourceLoader();
+    rl.add({ type: "image", key: "pic1", src: "1.jpg" });
+    rl.add({ type: "audio", key: "audio1", src: "01.mp3" });
+    rl.add({ type: "video", key: "video1", src: "01.mp4" }); 
+
+    rl.itemLoad = function () {
+        ok(true, "item " + this.type + " callback success");
+    }
+    rl.onload = function () {
+        ok(true, "all items callback success");
+        start();
+    }
+    rl.load();
+});
+
+asyncTest("user resource after load", function () {
+    var rl = new ResourceLoader();
+    rl.add({ type: "image", key: "pic1", src: "1.jpg" });
+    rl.add({ type: "audio", key: "audio1", src: "01.mp3" }); 
+
+    rl.onload = function () {
+        notEqual(rl.rs["pic1"], null);
+        notEqual(rl.rs["audio1"], null); 
+        start();
+    }
+    rl.load();
+});
+
+asyncTest("delete resource item test", function () {
+    var rl = new ResourceLoader();
+    rl.add({ type: "image", key: "pic1", src: "1.jpg" });
+
+    rl.onload = function () {
+        var length = rl.resourceList.length;
+
+        rl.clearItem("pic1");
+        equal(rl.rs["pic1"], null);
+        equal(rl.resourceList.length, length - 1, "resource list's length is minus 1");
+        start();
+    }
+    rl.load();
+});
+
+asyncTest("delete all resource item test", function () {
+    var rl = new ResourceLoader();
+    rl.add({ type: "image", key: "pic1", src: "1.jpg" });
+    rl.add({ type: "audio", key: "audio1", src: "01.mp3" }); 
+
+    rl.onload = function () {
+        rl.clearAll();
+        equal(rl.rs["pic1"], null);
+        equal(rl.rs["audio1"], null);
+        equal(rl.resourceList.length, 0, "resource list is empty after clearAll");
+        start();
+    }
+    rl.load();
+});
+
+asyncTest("call onload  while resource", function () {
+    var rs = new ResourceLoader();
+    rs.onload = function () {
+        ok(true, "yes! empty call!");
+        start();
+    }
+
+    rs.load();
+});
